85df3b710427bc36307bbab90e7649ac71b64340
dvdrlm <davidrolim704@gmail.com>
2025-02-20 11:47:04 +0000

    base de dados

--- dteaches.sql
+++ dteaches.sql
@@ -1,38 +1,31 @@
 CREATE DATABASE dteaches;
 
 CREATE TABLE users (
-    username VARCHAR(10) UNIQUE NOT NULL,
+    username VARCHAR(20) UNIQUE NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
-    pass VARCHAR(20) NOT NULL,
+    pass VARCHAR(30) NOT NULL,
     data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE licoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(50) NOT NULL,
-    conteudo VARCHAR(100) NOT NULL,
+    conteudo TEXT NOT NULL
 );
 
 CREATE TABLE expressoes (
     id_expressao INT PRIMARY KEY AUTO_INCREMENT,
-    id_categoria INT,
-    ingles VARCHAR(255),
-    traducao VARCHAR(255),
-    arquivo_audio VARCHAR(255),
-    FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria)
-);
-
-CREATE TABLE Aulas (
-    id_aula INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(255),
-    id_categoria INT,
-    conteudo TEXT,
-    FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria)
+    versao_ingles TEXT NOT NULL,
+    traducao_portugues TEXT NOT NULL,
+    id_categoria INT NOT NULL,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
 
 CREATE TABLE progresso (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    aula_id INT,
-    status ENUM('não iniciado', 'em andamento', 'concluído') NOT NULL,
-    FOREIGN KEY(aula_id) REFERENCES licoes(id)
+    username VARCHAR(20),
+    id_expressao INT,
+    completo BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (username, id_expressao),
+    FOREIGN KEY (username) REFERENCES users(username),
+    FOREIGN KEY (id_expressao) REFERENCES expressoes(id_expressao)
 );
